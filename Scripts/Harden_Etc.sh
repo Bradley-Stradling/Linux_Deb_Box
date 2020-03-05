fc4f6d175b2bbe2531920102ab3060dd019c1df9
Bradley-Stradling <bradstrad2@gmail.com>
2020-03-05 17:32:30 -0500

    Added Script for linux harden on etc perms

---
+++ Scripts/Harden_Etc.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#*******************************************************************************
+
+echo "This script is designed to run from root on a linux box with standard \
+    locations for etc/shadow, etc/gshadow, etc/group and etc/passwd. " ;
+
+
+chmod 600 /etc/shadow
+chmod 600 /etc/gshadow
+chmod 644 /etc/group
+chmod 644 /etc/passwd
+# I used 640 as I don't want this files veiwable by non users.
+
+echo -e "Permissions should be changed! see below \n"
+
+ls -la /etc/shadow /etc/gshadow /etc/group /etc/passwd
+
+
+
