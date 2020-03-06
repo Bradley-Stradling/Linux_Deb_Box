95a6f5b32395a3e177d9f7b704446e9faa80aa79
Bradley Stradling <45749708+Bradley-Stradling@users.noreply.github.com>
2020-03-05 20:42:46 -0600

    Update Back_Me_Up.sh
    Commentation

--- Scripts/Back_Me_Up.sh
+++ Scripts/Back_Me_Up.sh
@@ -2,7 +2,7 @@
 
 #*******************************************************************************
 
-# name of back up directory
+# name of new back up directory
 INPUT$1
 
 mkdir //home/bread/Back_ups_files/$1
