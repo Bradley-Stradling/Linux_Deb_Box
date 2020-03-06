6e223472b10d1be75639f5b5b01f2f412ee6b48d
Bradley Stradling <45749708+Bradley-Stradling@users.noreply.github.com>
2020-03-05 21:50:24 -0600

    Update user_batch_add.sh

--- Scripts/user_batch_add.sh
+++ Scripts/user_batch_add.sh
@@ -2,7 +2,15 @@
 
 #*******************************************************************************
 
-# this will be a more onvolved script to add x users and set groups for each 
+# this will be a more involved script to add x users and set groups for each 
 # and create x folders and give specific users.
 # Basically will take a list of names and flags and make new users and add 
 # them to the box with proper permissions and acess
+
+#no idea when I will get around to doing it
+
+#Enjoy the rabbit for now
+
+# (\_/)
+# (*.*)
+#(")_(")
