19a0c44eafca7b9ce813e14aae7bafcf85c0a548
Bradley Stradling <45749708+Bradley-Stradling@users.noreply.github.com>
2020-03-06 13:51:32 -0600

    Update and rename Find2.sh to Find_1_File.sh

--- Scripts/Find_1_File.sh
+++ Scripts/Find_1_File.sh
@@ -2,11 +2,10 @@
 
 #*******************************************************************************
 
-#find file script
+#find file script so I don't have to type out the flags
 
 INPUT1=$1 #file  string name 1
-INPUT2=$2 #file string name 2
 
-find -type f -iname *$1* -o -type f -iname *$2*
+find -type f -iname *$1*
 
 
