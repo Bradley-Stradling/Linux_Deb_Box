dac5fcc950249ba87615c19e05531191f59fba7d
Bradley-Stradling <bradstrad2@gmail.com>
2020-03-06 21:07:32 -0500

    fixed write to bash rc somehow ehco wrote the -e to file

--- Scripts/New_Paths.sh
+++ Scripts/New_Paths.sh
@@ -14,8 +14,6 @@
 
 # Needs error checking to ensure paths and directories were generated correctly?
 
-# 
-
 cwd=$(pwd)
 
 # Need to add prompt to ask for dependancies before script run y/n
@@ -50,7 +48,7 @@ cp .bashrc ${cwd}/${back_Up_Dir}/${bashrc_Dir}/bashrc.bak
 
 # add scripts directory to my path.
 echo "# Added path to my scripts directorys." >> .bashrc
-echo -e "export PATH=\"${cwd}/${scripts_Dir}:\$PATH\"" >> .bashrc
+echo "export PATH=\"${cwd}/${scripts_Dir}:\$PATH\"" >> .bashrc
 
 # add aliases file (will need manual connection from bash rc for now.)
 cp ${Repo_Path}/aliases .bash_aliases
