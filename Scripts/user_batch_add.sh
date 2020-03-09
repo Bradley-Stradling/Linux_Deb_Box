b39362bcfb0fc9f2aafbda41fc5648ab253790cb
Bradley Stradling <45749708+Bradley-Stradling@users.noreply.github.com>
2020-03-09 11:42:17 -0500

    Update user_batch_add.sh

--- Scripts/user_batch_add.sh
+++ Scripts/user_batch_add.sh
@@ -9,6 +9,18 @@
 
 #no idea when I will get around to doing it
 
+# need to set-up source file for user add with
+
+# username deafult_password? group x number of y flags.
+
+# script should should iteratively allow users to be added by writing flags
+# and variables 1 user per line to source 1 user at a time until
+# script is flagged to create users in source file
+# source files should be checked and users with details of how they'll
+# be created should be cofirmed in terminal output to user
+# maked back up of source file used to create users and then clean source
+# file at end of script
+
 #Enjoy the rabbit for now
 
 # (\_/)
