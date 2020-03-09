b04539352d978e74e0a4a85ee06fb01aae8d9eed
Bradley Stradling <45749708+Bradley-Stradling@users.noreply.github.com>
2020-03-09 14:21:20 -0500

    Update Unzip_and_Move.sh
    Added Header and notes of future work.

--- Scripts/Unzip_and_Move.sh
+++ Scripts/Unzip_and_Move.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
 #*******************************************************************************
+# (\_/)   Author: Bradley Stradling
+# (o.o)   Date of first revision: 3/6/20
+#(")_(")  Lincense: https://unlicense.org/
+#*******************************************************************************
+# (\_/)   Script to unpack .zip files at target location and return to 
+# (*.*)   current working directory, as well as back up .zip files and then
+#(")_(")  remove .zip files from target location.
+#*******************************************************************************
 
 # Script is broken until paths source is connected. then needs refactor.
 
 # !!!incomplete and needs more testing!!!
+
+# needs a lot of error checking and output confirmation to terminal of success
 
 cwd=${pwd}
 
