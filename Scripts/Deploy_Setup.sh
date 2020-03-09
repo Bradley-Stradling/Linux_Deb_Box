7aca8e61fe1cbe2797c663e1e0f9ee69f8a9c3c8
Bradley Stradling <45749708+Bradley-Stradling@users.noreply.github.com>
2020-03-09 09:25:06 -0500

    Update Deploy_Setup.sh
    Added terminal output to confirm directory, file and permission changes. Nothing is tested yet.

--- Scripts/Deploy_Setup.sh
+++ Scripts/Deploy_Setup.sh
@@ -34,10 +34,7 @@ bashrc_Dir=Set-up
 pathz_File=Paths.txt
 workz_File=Workpath.txt
 
-# array of files to be created
-list_Files=(
-'${pathz_File}'
-'${workz_File}'
+# array of files to be created here?
 
 #concatenating paths to easier to work with variables
 path_Source_Dir=${cwd}/${source_Dir}
@@ -50,7 +47,7 @@ list_Directories=(
 '${path_Source_Dir}'
 '${path_Test_Dir}'
 '${path_Scripts_Dir}'
-'${path_Back_Up_Dir'}
+'${path_Back_Up_Dir}'
 '${path_Back_Up_Dir}/${bashrc_Dir}'
 )
 
@@ -81,7 +78,8 @@ done
 # needs further testing and likely if confirmation message
 for directory in $(ls ${cwd});
 do
-chmod 700 ${path_Scripts_Dir}/$directory
+chmod 600 ${path_Scripts_Dir}/${directory}
+echo "Permission set to ${directory} to user only read write"
 done
 
 touch ${path_Sources_Dir}/${pathz_File}
@@ -105,37 +103,64 @@ echo "back_Up_Files_Path=${path_Back_Up_Dir}" >> ${path_Sources_Dir}/${pathz_Fil
 # target for change work to change work target with go work alias
 echo "work_Path_File=${path_Sources_Dir}/${workz_File}" >> ${path_Sources_Dir}/${pathz_File}
 
+# confrim to terminal output paths written to files
+echo "source_Path=${path_Sources_Dir} written to ${path_Sources_Dir}/${pathz_File}"
+echo "test_Path=${path_Test_Dir} written to ${path_Sources_Dir}/${pathz_File}"
+echo "scripts_Path=${path_Scripts_Dir} written to ${path_Sources_Dir}/${pathz_File}"
+echo "back_Up_Files_Path=${path_Back_Up_Dir} written to ${path_Sources_Dir}/${pathz_File}"
+# target for change work to change work target with go work alias
+echo "work_Path_File=${path_Sources_Dir}/${workz_File} written to ${path_Sources_Dir}/${pathz_File}"
+
 # back up the .bashrc
 cp .bashrc ${path_Back_Up_Dir}/${bashrc_Dir}/bashrc.bak
+# confirm to terminal back up of bashrc
+echo ".bashrc backed up to ${path_Back_Up_Dir}/${bashrc_Dir}/bashrc.bak"
 
 # add scripts directory to my path.
 echo "# Added path to my scripts directorys." >> .bashrc
 echo "export PATH=\"${path_Scripts_Dir}:\$PATH\"" >> .bashrc
 echo "source ${path_Sources_Dir}/${pathz_File}" >> .bashrc
 
+# confrim what is appended to bashrc
+echo "# Added path to my scripts directorys. written to .bashrc"
+echo "export PATH=\"${path_Scripts_Dir}:\$PATH\" written to .bashrc"
+echo "source ${path_Sources_Dir}/${pathz_File} written to .bashrc"
+
+
 # add aliases file
 cp ${Repo_Path}/aliases .bash_aliases
+# confirm to terminal aliases file copied
+if [ -f .bash_aliases];
+then
+echo "${Repo_Path}/aliases copied to .bash_aliases"
+else
+echo "${Repo_Path}/aliases was not copied to .bash_aliases"
 
 # prepend source of path file to aliases file
 echo "source ${path_Sources_Dir}/${pathz_File}" | cat - .bash_aliases > temp \
  && mv temp .bash_aliases
+echo "source ${path_Sources_Dir}/${pathz_File} prepended to .bash_aliases"
 # prepend
 echo "source ${path_Sources_Dir}/${workz_File}" | cat - .bash_aliases > temp \
  && mv temp .bash_aliases
+echo "source ${path_Sources_Dir}/${workz_File} prepended to .bash_aliases."
 
 # add cp of scripts to Scripts folder from repo
 cp ${Repo_Path}/Scripts/* ${path_Scripts_Dir}
+echo "${Repo_Path}/Scripts/ copied to ${path_Scripts_Dir}"
 
 # attempting to prepend source file to all scripts 
 for script in $(ls ${path_Scripts_Dir});
 do
 echo "source ${path_Sources_Dir}/${pathz_File}" | cat - ${path_Scripts_Dir}/$script > \
 ${path_Scripts_Dir}/temp && mv ${path_Scripts_Dir}/temp ${path_Scripts_Dir}/$script
+echo "source ${path_Sources_Dir}/${pathz_File} prepended to ${path_Scripts_Dir}/$script"
 done
 
 for script in $(ls ${path_Scripts_Dir});
 do
 chmod 700 ${path_Scripts_Dir}/$script
+echo "Permission set to ${path_Scripts_Dir}/$script to user only read write execute"
 done
 
 # need to add removal of common unwanted directories
