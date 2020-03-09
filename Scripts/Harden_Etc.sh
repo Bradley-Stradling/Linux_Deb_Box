afad2c79c71ae30913bd32aa85b2e75b9ed28403
Bradley Stradling <45749708+Bradley-Stradling@users.noreply.github.com>
2020-03-09 14:13:32 -0500

    Update Harden_Etc.sh
    Added conditional to ask user if they are root.  ... Need to add hard check if user is root later.

--- Scripts/Harden_Etc.sh
+++ Scripts/Harden_Etc.sh
@@ -9,6 +9,16 @@ echo "This script is designed to run from root on a linux box with standard \
     locations for etc/shadow, etc/gshadow, etc/group and etc/passwd. " ;
 # need to add contional to check if user is in root if not exit script
 
+read -p "Are u running this as root? y/n -->" runasroot
+
+# this is not dummy proof but will work for now, should be more verbose check on
+# what the user calling this script is logged in as.
+if [ $runasroot -ne y];
+then
+echo "Script must be run as root. Exiting."
+exit
+fi
+
 chmod 600 /etc/shadow
 chmod 600 /etc/gshadow
 chmod 644 /etc/group
