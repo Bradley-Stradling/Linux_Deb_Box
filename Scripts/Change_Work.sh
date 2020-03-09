c30abe401fb01056adfa8c7bc60873b2e3526fc4
Bradley Stradling <45749708+Bradley-Stradling@users.noreply.github.com>
2020-03-09 13:41:23 -0500

    Update Change_Work.sh
    script is more verbose now. error if passed target directory exists check in place.

--- Scripts/Change_Work.sh
+++ Scripts/Change_Work.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
 #*******************************************************************************
+# (\_/)   Author: Bradley Stradling
+# (o.o)   Date of first revision: 3/6/20 (Refactoring ongoing)
+#(")_(")  Lincense: https://unlicense.org/
+#*******************************************************************************
+# (\_/)   This script is intended to change the sourced variable of the gowork
+# (*.*)   alias to the input'd full path.
+#(")_(")  
+#*******************************************************************************
 
-read -p "Enter the new work spot ==>" werkpath
+# incomplete and untested as is.
 
-echo  "work_Path=${werkpath}" > ${work_Path_File}
+read -p "Enter the full path to the new work spot ==>" werkpath
 
+if [ -d ${werkpath} ];
+  then
+    echo "work_Path=${werkpath}" > ${work_Path_File}
+    echo "work_Path=${werkpath} written to ${work_Path_File}"
+  else
+    echo "Unable to locate ${werkpath}. ${work_Path_File} is unchanged."
+    exit
+fi
