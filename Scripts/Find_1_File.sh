d3e359fe92e5d051786e386100b00db5b0dc8e0b
Bradley Stradling <45749708+Bradley-Stradling@users.noreply.github.com>
2020-03-09 13:55:45 -0500

    Update Find_1_File.sh
    Added comment header. Script needs to be more verbose  in information output to be useful otherwise why not just locate file..
    Possibly list all files named as such and their respective location?

--- Scripts/Find_1_File.sh
+++ Scripts/Find_1_File.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
 #*******************************************************************************
+# (\_/)   Author: Bradley Stradling
+# (o.o)   Date of first revision: 3/6/20
+#(")_(")  Lincense: https://unlicense.org/
+#*******************************************************************************
+# (\_/)   Script to shorthand locate and find a single file verbosely from the 
+# (*.*)   home directory without having to leave the current working directory.
+#(")_(")  
+#*******************************************************************************
 
-#find file script so I don't have to type out the flags
+# incomplete and broken as is !!!
 
-INPUT1=$1 #file  string name 1
+# want to grab user input move to home directory locate the full path to the file
+# and return to the current working directory after outputing the files full 
+# location and name Possibly list all files with duplicate names
+# and their respective location? More work to be done.
+
+read -p "Please enter the name of the file you want to find." file_Name
+
+# this might not be the way to do this but want to redirect output to variable
+locate ${file_Name}
 
 find -type f -iname *$1*
 
