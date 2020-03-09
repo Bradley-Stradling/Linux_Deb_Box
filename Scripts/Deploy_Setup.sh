d92996853430d8e805d170ccd2ef676dba2d16c0
Bradley-Stradling <bradstrad2@gmail.com>
2020-03-08 20:05:31 -0400

    Changed name of setup script and concatenated variables

--- Scripts/Deploy_Setup.sh
+++ Scripts/Deploy_Setup.sh
@@ -23,65 +23,71 @@ source_Dir=Sources
 test_Dir=Test_Boxs
 scripts_Dir=My_Scripts
 back_Up_Dir=Back_Up_Files
+
 bashrc_Dir=Set-up
+
 pathz_File=Paths.txt
 workz_File=Workpath.txt
 
-# Need to concantenate paths into single variables for easier reading and editing
+path_Source_Dir=${cwd}/${source_Dir}
+path_Test_Dir=${cwd}/${test_Dir}
+path_Scripts_Dir=${cwd}/${scripts_Dir}
+path_Back_Up_Dir=${cwd}/${back_Up_Dir}
+
 
 # make new directories
-mkdir ${cwd}/${source_Dir}
-mkdir ${cwd}/${test_Dir}
-mkdir ${cwd}/${scripts_Dir}
-mkdir ${cwd}/${back_Up_Dir}
-mkdir ${cwd}/${back_Up_Dir}/${bashrc_Dir}
+mkdir ${path_Sources_Dir}
+mkdir ${path_Test_Dir}
+mkdir ${path_Scripts_Dir}
+mkdir ${path_Back_Up_Dir}
+mkdir ${path_Back_Up_Dir}/${bashrc_Dir}
 
 # If error check here for directories
 
-touch ${cwd}/${source_Dir}/${pathz_File}
+touch ${path_Sources_Dir}/${pathz_File}
+touch ${path_Sources_Dir}/${workz_File}
 
-# Change work script needs to be targeted to the file
-touch ${cwd}/${source_Dir}/${workz_File}
+# If error check here for created files
 
 # write variable for paths to path source file
-echo "source_Path=${cwd}/${source_Dir}" >> ${cwd}/${source_Dir}/${pathz_File}
-echo "test_Path=${cwd}/${test_Dir}" >> ${cwd}/${source_Dir}/${pathz_File}
-echo "scripts_Path=${cwd}/${scripts_Dir}" >> ${cwd}/${source_Dir}/${pathz_File}
-echo "back_Up_Files_Path=${cwd}/${back_Up_Dir}" >> ${cwd}/${source_Dir}/${pathz_File}
+echo "source_Path=${path_Sources_Dir}" >> ${path_Sources_Dir}/${pathz_File}
+echo "test_Path=${path_Test_Dir}" >> ${path_Sources_Dir}/${pathz_File}
+echo "scripts_Path=${path_Scripts_Dir}" >> ${path_Sources_Dir}/${pathz_File}
+echo "back_Up_Files_Path=${path_Back_Up_Dir}" >> ${path_Sources_Dir}/${pathz_File}
 # target for change work to change work target with go work alias
-echo "work_Path_File=${cwd}/${source_Dir}/${workz_File}" >> ${cwd}/${source_Dir}/${pathz_File}
+echo "work_Path_File=${path_Sources_Dir}/${workz_File}" >> ${path_Sources_Dir}/${pathz_File}
 
 # back up the .bashrc
-cp .bashrc ${cwd}/${back_Up_Dir}/${bashrc_Dir}/bashrc.bak
+cp .bashrc ${path_Back_Up_Dir}/${bashrc_Dir}/bashrc.bak
 
 # add scripts directory to my path.
 echo "# Added path to my scripts directorys." >> .bashrc
-echo "export PATH=\"${cwd}/${scripts_Dir}:\$PATH\"" >> .bashrc
-echo "source ${cwd}/${source_Dir}/${pathz_File}" >> .bashrc
+echo "export PATH=\"${path_Scripts_Dir}:\$PATH\"" >> .bashrc
+echo "source ${path_Sources_Dir}/${pathz_File}" >> .bashrc
 
 # add aliases file
 cp ${Repo_Path}/aliases .bash_aliases
 
 # prepend source of path file to aliases file
-echo "source ${cwd}/${source_Dir}/${pathz_File}" | cat - .bash_aliases > temp \
+echo "source ${path_Sources_Dir}/${pathz_File}" | cat - .bash_aliases > temp \
  && mv temp .bash_aliases
 # prepend
-echo "source ${cwd}/${source_Dir}/${workz_File}" | cat - .bash_aliases > temp \
+echo "source ${path_Sources_Dir}/${workz_File}" | cat - .bash_aliases > temp \
  && mv temp .bash_aliases
 
 # add cp of scripts to Scripts folder from repo
-cp ${Repo_Path}/Scripts/* ${cwd}/${scripts_Dir}
+cp ${Repo_Path}/Scripts/* ${path_Scripts_Dir}
 
 # attempting to prepend source file to all scripts 
-for script in $(ls ${cwd}/${scripts_Dir});
+for script in $(ls ${path_Scripts_Dir});
 do
-echo "source ${cwd}/${source_Dir}/${pathz_File}" | cat - ${cwd}/${scripts_Dir}/$script > \
-${cwd}/${scripts_Dir}/temp && mv ${cwd}/${scripts_Dir}/temp ${cwd}/${scripts_Dir}/$script
+echo "source ${path_Sources_Dir}/${pathz_File}" | cat - ${path_Scripts_Dir}/$script > \
+${path_Scripts_Dir}/temp && mv ${path_Scripts_Dir}/temp ${path_Scripts_Dir}/$script
 done
 
-for script in $(ls ${cwd}/${scripts_Dir});
+for script in $(ls ${path_Scripts_Dir});
 do
-chmod 700 ${cwd}/${scripts_Dir}$script
+chmod 700 ${path_Scripts_Dir}/$script
 done
 
 # need to add removal of common unwanted directories
