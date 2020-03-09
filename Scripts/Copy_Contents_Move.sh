44cd79b4710578f4d8237cf9a1b884d58dd9e5b5
Bradley Stradling <45749708+Bradley-Stradling@users.noreply.github.com>
2020-03-09 13:47:35 -0500

    Update Copy_Contents_Move.sh
    Comment header added. error check for target added.

--- Scripts/Copy_Contents_Move.sh
+++ Scripts/Copy_Contents_Move.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
 
 #*******************************************************************************
+# (\_/)   Author: Bradley Stradling
+# (o.o)   Date of first revision: 3/6/20
+#(")_(")  Lincense: https://unlicense.org/
+#*******************************************************************************
+# (\_/)   This script is intended to copy the contents of the current working
+# (*.*)   directory and move it to the passed target directory.
+#(")_(")  
+#*******************************************************************************
 
-INPUT$!
+# incomplete and untested as is !!!
 
-# copys all contents of current directory to passed target directory
-cp -r * $1
+cwd=$(pwd)
+
+read -p "Please enter the full path to copy the contents of the current working \
+directory of ${cwd} to. -->" target_Dir 
+
+if [ -d ${target_Dir} ];
+  then
+    cp -r * ${target_Dir}
+    echo "Contents of ${cwd} copied to ${target_Dir}"
+  else
+    echo "Unable to path to ${target_Dir} exiting script."
+    exit
+fi
