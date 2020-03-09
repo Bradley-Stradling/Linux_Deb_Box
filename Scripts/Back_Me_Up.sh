b37934b6f2c25068b10af0dd898aa965cd74c9b9
Bradley Stradling <45749708+Bradley-Stradling@users.noreply.github.com>
2020-03-09 13:35:55 -0500

    Update Back_Me_Up.sh
    Added comment header and noted possible future changes.

--- Scripts/Back_Me_Up.sh
+++ Scripts/Back_Me_Up.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
 #*******************************************************************************
+# (\_/)   Author: Bradley Stradling
+# (o.o)   Date of first revision: 3/6/20
+#(")_(")  Lincense: https://unlicense.org/
+#*******************************************************************************
+# (\_/)   This script is meant to be called to back up the entire contents of
+# (*.*)   the current directory to a new directory with the passed name within
+#(")_(")  the back up directory.
+#*******************************************************************************
+
+# incomplete and untested as is !!!
+
+# need to add error checking and maybe change arugment pass to a prompt
 
 # name of new back up directory
 INPUT$1
