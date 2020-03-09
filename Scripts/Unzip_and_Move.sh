a5cff6b7c6930eaa9698e2f47b02d94b96b11a39
Bradley Stradling <45749708+Bradley-Stradling@users.noreply.github.com>
2020-03-09 14:29:49 -0500

    Update Unzip_and_Move.sh
    Added check if target directory could be found. notated future rework in comment.

--- Scripts/Unzip_and_Move.sh
+++ Scripts/Unzip_and_Move.sh
@@ -22,6 +22,17 @@ zip_Back_Up=$(date)_zips
 
 read -p "Enter the location of the .zips to unpack -->" zip_Files
 
+if [ !-d ${zip_Files} ];
+then
+  echo "Path to ${zip_Files} not found, exiting script."
+  exit
+fi
+
+# this needs to be reworked to copy .zip files to back up as well as currrent directory
+# then unpack them in current directory and delete the .zips from current directory
+# to maintain redunancy against file loss as the .zips should exist in back up as
+# well as still be in the target.
+
 cd $zip_Files
 
 mkdir ${back_Up_Files_Path}/${zip_Back_Up}
