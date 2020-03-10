b434823d652ad9f5b85ace091eb18072c561d48f
Bradley Stradling <45749708+Bradley-Stradling@users.noreply.github.com>
2020-03-10 08:02:58 -0500

    Create Pack_Up_Setup.sh

---
+++ Scripts/Pack_Up_Setup.sh
@@ -0,0 +1,12 @@
+#*******************************************************************************
+# (\_/)   Author: Bradley Stradling
+# (o.o)   Date of first revision: 3/10/20 (Refactoring ongoing)
+#(")_(")  Lincense: https://unlicense.org/
+#*******************************************************************************
+# (\_/)   Script to pack up my set-up. Copy scripts and aliases to repo and
+# (*.*)   clean up the sourcing in them. Should only grab scripts and aliases.
+#(")_(")  Should confirm each change to the repo before finalization.
+#*******************************************************************************
+
+# Placeholder for packing up new scripts and updating aliases list.
+# Not sure how I want to tackle this yet but wanted to note it as future work.
