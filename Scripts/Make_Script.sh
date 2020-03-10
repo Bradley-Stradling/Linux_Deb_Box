4d6a7b7dedd684f76fcdc81bd7a5ae4015771b71
Bradley Stradling <45749708+Bradley-Stradling@users.noreply.github.com>
2020-03-10 08:24:25 -0500

    Update Make_Script.sh
    removed nano open of new script as prefer a different editor. Added some confirmation conditionals.

--- Scripts/Make_Script.sh
+++ Scripts/Make_Script.sh
@@ -16,9 +16,16 @@
 
 INPUT1=$1 # name for new script
 
-touch ${scripts_Path}/$1.sh
+read -p "Create new script with filename of ${1}.sh? y/n -->" create
 
-chmod 700 ${scripts_Path}/$1.sh
+if [ ${create} == 'y' ];
+  then
+    touch ${scripts_Path}/$1.sh
+    echo "${1}.sh has been created within ${scripts_Path}."
+  else
+    echo "Whoops nevermind exiting script..."
+    exit
+fi
 
 echo "#!/bin/bash" > ${scripts_Path}/$1.sh
 
@@ -34,5 +41,16 @@ echo -e "# (\_/)\t\n# (*.*)\t\n#(\")_(\")\t"
 
 echo "#*******************************************************************************" >> ${scripts_Path}/$1.sh
 
+echo "Standard comment header written to ${1}.sh"
 
-nano ${scripts_Path}/$1.sh
+read -p "Set script to user only read, write, and execute? y/n -->" perm_Set
+
+if [ ${perm_Set} == 'y' ];
+  then
+    chmod 700 ${scripts_Path}/$1.sh
+    echo "${1}.sh has been set to only user read write execute."
+  else
+  echo "You will have to change the permissions on ${1}.sh later on your own to run it..."
+  exit
+fi
+    
