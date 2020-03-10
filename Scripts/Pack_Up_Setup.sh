844d21b1d91fdf9c10d93108f1f0dbddee9bf644
Bradley Stradling <45749708+Bradley-Stradling@users.noreply.github.com>
2020-03-10 13:37:54 -0500

    Update Pack_Up_Setup.sh
    Laid out enough of script to test basic functionality.

--- Scripts/Pack_Up_Setup.sh
+++ Scripts/Pack_Up_Setup.sh
@@ -8,6 +8,47 @@
 #(")_(")  Should confirm each change to the repo before finalization.
 #*******************************************************************************
 
-# Placeholder for packing up new scripts and updating aliases list.
-# Not sure how I want to tackle this yet but wanted to note it as future work.
+# incomplete and untested as is !!!
+
+# need to refactor to show what scripts have been changed.
+
 # Script should sanitize files to read write only permissions.
+# variable to scripts from source path is scripts_Path
+# variable to repo from source path is repo_Path
+
+read - p "Pack up and replace alises list in repo? y/n -->" pack_Aliases
+
+if [ ${pack_Aliases} == 'y' ];
+then
+  echo "Copying .bash_aliases to ${repo_Path} and removing source."
+  cp ${HOME}/.bash_aliases ${repo_Path}
+#need to add code here to remove lines containing source from aliases file in local repo
+  echo "Copied .bash_aliases to ${repo_Path} and removed source lines."
+else
+  echo "Leaving aliases file alone, new aliases not added to the repo."
+fi
+
+read -p "Do you want to add all scripts to local repo replacing any scripts that \
+were changed? y/n -->" pack_Scripts
+
+if [ ${pack_Scripts} == 'y'];
+then
+  for script in (ls ${scripts_Path}) # need to check this syntax
+    do
+        echo "Copying ${script} to ${repo_Path}/Scripts"
+        cp ${script} ${repo_Path}/Scripts
+# code to remove lines with source from each script in local repo here is needed.
+        echo "${script} copied to ${repo_Path}/Scripts and source lines removed."
+    done
+else
+  echo "Local scripts will not be copied to the local repo."
+ fi
+ 
+ if [ ${pack_Scripts} == 'y'];
+ then
+    for script in (ls ${repo_Path}/Scripts)
+      do
+          chmod 666 ${script}
+          echo "Set permission in ${repo_Path}/Scripts for ${script} to read write only for all."
+      done
+fi
