3fb200fe7578c56730ef036e5de4416e262294d8
Bradley Stradling <45749708+Bradley-Stradling@users.noreply.github.com>
2020-03-10 08:09:49 -0500

    Create Resolution_Set.sh
    Added placeholder for resolution set.

---
+++ Scripts/Resolution_Set.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#*******************************************************************************
+# (\_/)   Author: Bradley Stradling
+# (o.o)   Date of first revision: 3/6/20
+#(")_(")  Lincense: https://unlicense.org/
+#*******************************************************************************
+# (\_/)   Script to create and set a new mode to xrandr for a resolution not
+# (*.*)   already in it's current scope. Specifically want to have 1920x1080
+#(")_(")  added to resolution list. Script should be able to create any though.
+#*******************************************************************************
+
+# Place holder for future work to be done.
+
+# want a quick and easy way to add a new resolution setting to a box.
