aa2878dd534786400d604bc813ced449f6f2fbec
Bradley Stradling <45749708+Bradley-Stradling@users.noreply.github.com>
2020-03-10 14:44:49 -0500

    Update Make_Script.sh
    Added auto generation of date and the current license my scripts are written under.

--- Scripts/Make_Script.sh
+++ Scripts/Make_Script.sh
@@ -33,7 +33,7 @@ echo "" >> ${scripts_Path}/$1.sh
 
 echo "#*******************************************************************************" >> ${scripts_Path}/$1.sh
 
-echo -e "#  (\_/)\tAuthor: Bradley Stradling\n#  (o.o) Hello!\tDate of first revision:\n# (\")_(\")\tLincense:" >> ${scripts_Path}/$1.sh
+echo -e "#  (\_/)\tAuthor: Bradley Stradling\n#  (o.o) Hello!\tDate of first revision: $(date)\n# (\")_(\")\tLincense: https://unlicense.org/" >> ${scripts_Path}/$1.sh
 
 echo "#*******************************************************************************" >> ${scripts_Path}/$1.sh
 
