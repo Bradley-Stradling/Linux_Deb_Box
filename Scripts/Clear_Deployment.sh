b94811dd9557b860fd8659bddad6d582b634bb64
Bradley Stradling <45749708+Bradley-Stradling@users.noreply.github.com>
2020-05-23 18:34:27 -0500

    Create Clear_Deployment.sh

---
+++ Scripts/Clear_Deployment.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#*******************************************************************************
+# (\_/)   Author: Bradley Stradling
+# (o.o)   Date of first revision: 3/10/20
+#(")_(")  Lincense: https://unlicense.org/
+#*******************************************************************************
+# (\_/)   Script to append new alias with comment to .bash_aliases.
+# (*.*)   Should be able to run this on the fly from any directory append
+#(")_(")  the inputs to the aliases file and return to wherever you were.
+#*******************************************************************************
+
+# Need to have this script remove all files and directories deployed by 
+# this repos deployment script.
+
