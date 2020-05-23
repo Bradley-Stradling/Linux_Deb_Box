11e246e1dd08f6de80bb6e7aa3db3cc86c18b3cd
Bradley Stradling <45749708+Bradley-Stradling@users.noreply.github.com>
2020-05-23 18:32:51 -0500

    Update Resolution_Set.sh
    Still needs work

--- Scripts/Resolution_Set.sh
+++ Scripts/Resolution_Set.sh
@@ -12,7 +12,8 @@
 
 # incomplete and untested as is
 
-echo -p "Do you want to add 1920px1080p 60hz to xrandr? y/n-->" add_Rez
+
+echo -p "Do you want to add 1920px1080p 60hz to xrandr? y/n-->" setup_Check
 
 if [ $setup_Check != 'y' ];
 then
@@ -23,7 +24,7 @@ fi
 
 echo -e "Executing \"xrandr --newmode "1920x1080_60.00"  172.80  1920 2040 2248 2576  1080 1081 1084 1118  -HSync +Vsync\""
 
-xrandr --newmode "1920x1080_60.00"  172.80  1920 2040 2248 2576  1080 1081 1084 1118  -HSync +Vsync
+xrandr --newmode 1920x1080_60.00 172.80  1920 2040 2248 2576  1080 1081 1084 1118  -HSync +Vsync
 
 echo -p "Please enter the name of the output in xrandr to add the new mode to. -->" mode_Name
 
