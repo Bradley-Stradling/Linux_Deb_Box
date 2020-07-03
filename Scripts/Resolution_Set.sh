e0c49ea7ad053f021bd3059c242407c33c34769a
Bradley Stradling <45749708+Bradley-Stradling@users.noreply.github.com>
2020-07-02 22:41:23 -0500

    Update Resolution_Set.sh
    Individual commands are working time to test.

--- Scripts/Resolution_Set.sh
+++ Scripts/Resolution_Set.sh
@@ -28,5 +28,7 @@ xrandr --newmode 1920x1080_60.00 172.80  1920 2040 2248 2576  1080 1081 1084 111
 
 echo -p "Please enter the name of the output in xrandr to add the new mode to. -->" mode_Name
 
-echo -e "executing \"xrandr --addmode ${mode_Name} "1920x1080_60.00\""
+echo -e "Executing \"xrandr --addmode ${mode_Name} "1920x1080_60.00\""
+
+xrandr --addmode ${mode_Name} "1920x1080_60.00"
 
