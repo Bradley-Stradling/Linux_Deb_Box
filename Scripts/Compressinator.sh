6f2d636020f01858708ba4564f81f30af03513a0
Bradley Stradling <45749708+Bradley-Stradling@users.noreply.github.com>
2020-09-09 13:11:07 -0500

    Delete Compressinator.sh
    cleaning it up

--- Scripts/Compressinator.sh
+++
@@ -1,4 +0,0 @@
-Place holder for a script to compress a file x number of times, and the reverse as well.
-
-Preferably specifying different compression methods, possibly even allowing for differing series of methods.
-
