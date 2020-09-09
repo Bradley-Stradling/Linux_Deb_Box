3d6185dea3b71a8fee38ce847ae6db3e2dc5291a
Bradley Stradling <45749708+Bradley-Stradling@users.noreply.github.com>
2020-09-09 13:09:43 -0500

    Update Add_Alias.sh
    cleaning it up

--- Scripts/Add_Alias.sh
+++ Scripts/Add_Alias.sh
@@ -10,24 +10,17 @@
 #(")_(")  the inputs to the aliases file and return to wherever you were.
 #*******************************************************************************
 
-# Script tested and debugged  works :3
-
 cwd=$(pwd)
 
-# move to home directory (may want to just add path to home to file output)
-# or source path to home directory through variable
 cd $HOME
 
-# check if .bash_aliases was located properly.
 if [ ! -f .bash_aliases ]; # need to check syntax here
 then
 echo "was not able to locate .bash_aliases in ${HOME} please check script. Exiting."
-#return to working directory
 cd ${cwd}
 exit 1
 fi
 
-# need to check that spaces in input and full string is passed to comment variable
 read -p "What will this new alias do? (comment) -->" comment
 read -p "What will call this alias? -->" alias_Call
 read -p "what will this alias do? -->" alias_Do
@@ -46,14 +39,11 @@ echo -e "alias ${alias_Call}='${alias_Do}'" >> .bash_aliases
 echo " "
 echo "# ${comment} appended to .bash_aliases"
 echo -e "alias ${alias_Call}='${alias_Do}' appened to to .bash_aliases"
-#reset bash enviroment to pick up change
 exec bash
-#return to working directory
 cd ${cwd}
 exit 1
 else
 echo "Whoops check the script or your inputs and try running again. Exiting."
-#return to working directory
 cd ${cwd}
 exit 1
 fi
