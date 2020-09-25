de3cb4331829da7a3914cf41b72a743a56d3955a
Bradley Stradling <45749708+Bradley-Stradling@users.noreply.github.com>
2020-09-25 11:38:58 -0500

    Update Bash_Script_Help.sh
    saving commit to prevent work loss, outputs are untested, need to check format

--- Scripts/Bash_Script_Help.sh
+++ Scripts/Bash_Script_Help.sh
@@ -18,9 +18,29 @@ echo "#**************************************************************"
 
 echo -e "\n"
 
+echo "credit to https://devhints.io/bash for the info"
+
 echo -e "\"1\" basic loop\t\t \"2\" c-like loop\t \"3\" ranged loops\t"
 
-echo -e "\"4\" line read loop\t\t \"5\" while loop\t \"6\" condtionals\t"
+echo -e "\"4\" line read loop\t\t \"5\" while loop\t \"6\" conditionals\t"
+
+echo -e "\"7\" basic function syntax and call\t \"8\" brace expansion\t \"9\" arguments\t"
+
+echo -e "\"10\" file conditionals\t \"11\" defining arrays\t \"12\" working on arrays\t"
+
+echo -e "\"13\" array operations\t \"14\" defining dictionaries\t \"15\" working on dictionaries\t"
+
+echo -e "\"16\" dictionary iteration\t \"17\" options\t \"18\" glob options\t"
+
+echo -e "\"19\" history commands\t \"20\" history expansion\t \"21\" history operations\t"
+
+echo -e "\"22\" history slices\t \"23\" subshells\t \"24\" isnpect a command\t"
+
+echo -e "\"25\" history slices\t \"26\" subshells\t \"27\" isnpect a command\t"
+
+echo -e "\"28\" redirection\t \"29\" trap errors\t \"30\" case/switch\t"
+
+echo -e "\"31\" special variables\t \"32\" check cmd result\t \"33\" grep check\t"
 
 read -p "What can the rabbit assist you with on this fine day? <--" output_Choice
 
@@ -38,6 +58,10 @@ case $output_Choice in
 		echo -e "for i in {5..10}; do\n\techo \"Stay a while and listen $i\"\ndone"
 		echo "With a step size of 3"
 		echo -e "for i in {12..36..3}; do\n\techo \"Stay a while and listen $i\"\ndone"
+		echo "iterating an array"
+		echo -e "for i in \"${array_Name[@]}\"; do"
+		echo "cmd $i"
+		echo "done"
 	;;
 	4)
 		echo -e "cat file | while read line; do\n\t echo $line"
@@ -46,6 +70,12 @@ case $output_Choice in
 		echo -e "while [[ \"$A\" == \"$B\" ]]; do\n\t {perform an action}\n done"
 	;;
 	6)
+		echo -e "if [[ -flag \"$variable\" ]]; then"
+		echo -e "\t cmd \"do a thing\""
+		echo -e "elif [[ -anotherflag \"$variable\" ]]; then"
+		echo -e "\t cmd \"do a thing\""
+		echo "fi"
+		
 		echo -e "[[ is a command that returns a boolean value"
 		echo -e "[ and [[ perform a similar function but [[ should be prefered "
 		echo -e "[[ -z STRING ]]	Empty string"
@@ -64,6 +94,109 @@ case $output_Choice in
 		echo -e "[[ ! EXPR ]]	Not"
 		echo -e "[[ X && Y ]]	And"
 		echo -e "[[ X || Y ]]	Or"
+	;;
+	7)
+		echo -e "do_A_Thing() {"
+		echo -e "\tdoing a thing"
+		echo -e "}"
+		echo -e "cmd $(do_A_Thing)"
+		
+		echo -e "passing values into function, \"function\" can be used to declare a function but not needed"
+		echo "function do_A_Thing() {"
+		echo -e "cmd \"do a thing with $1\""
+		echo "}"
+		echo -e "do_A_Thing \"value\""
+	;;
+	8)
+		echo "for all files in array/list from A to C, (A.ext B.ext, C.ext)"
+		echo "cmd {A,C}.ext"
+		echo "for all numbers in range of 15 to 20"
+		echo "{15..20}"
+		echo "for brace expansion with variable step size"
+		echo "{50..200..25}
+		echo "same as 50 75 100 125 150 175 200"
+	;;
+	9)
+		echo "Arguments as below"
+		echo -e "$#\tnumber of arguments"
+		echo -e "$*\tall arguments"
+		echo -e "$@\tall arguments from first"
+		echo -e "$(number)\t1st, 2nd, 3rd etc argument"
+		echo -e "$_\tlast argument of previous cmd"
+	;;
+	10)
+		echo "File Conditionals"
+		echo -e "[[ -e FILE ]]	Exists?"
+		echo -e "[[ -r FILE ]]	Readable?"
+		echo -e "[[ -h FILE ]]	Symlink?"
+		echo -e "[[ -d FILE ]]	Directory?"
+		echo -e "[[ -w FILE ]]	Writable?"
+		echo -e "[[ -s FILE ]]	Size is > 0 bytes?"
+		echo -e "[[ -f FILE ]]	is a File?"
+		echo -e "[[ -x FILE ]]	Executable?"
+		echo -e "[[ FILE1 -nt FILE2 ]]	1 is more recent than 2?"
+		echo -e "[[ FILE1 -ot FILE2 ]]	2 is more recent than 1?"
+		echo -e "[[ FILE1 -ef FILE2 ]]	Same files?"
+	;;
+	11)
+		echo "array_Name=(value_1, value_2, value_3)"
+		echo "or this can be performed by"
+		echo -e array_Name=\"value_1\"
+		echo -e array_Name=\"value_2\"
+		echo -e array_Name=\"value_3\"
+	;;
+	12)
+		echo -e "cmd ${array_Name[3]}\t 3rd element"
+		echo -e "cmd ${array_Name[-2]}\t 2nd from last element"
+		echo -e "cmd ${array_Name[@]}\t all elements space seperation"
+		echo -e "cmd ${#array_Name[@]}\t numbers of elements"
+		echo -e "cmd ${#array_Name}\t string length of 1st element"
+		echo -e "cmd ${#array_Name[n]}\t string length of nth element"
+		echo -e "cmd ${array_Name[@]:p:l}\t range from position p to length l"
+		echo -e "cmd ${!array_Name[@]}\t key of all elements space seperated"
+	;;
+	13)
+		echo "I need to work on array operations more before i make note of them"
+	;;
+	14)
+		echo "aka associative arrays, hence -A"
+		echo "declare -A genre"
+		echo -e "genre[AngelMaker]\"Deathcore\""
+		echo -e "genre[Metallica]\"Metal\""
+		echo -e "genre[LambofGod]\"Groove Metal\""
+	;;
+	15)
+		echo -e "${genre[AngelMaker]}\t# "
+		echo -e "${genre[@]}\t# all values"
+		echo -e "${!genre[@]}\t# all keys"
+		echo -e "${#genre[@]}\t# number of elements"
+		echo -e "unset genre[Metallica]\t# delete Metallica"
+	;;
+	16)
+		echo "need to check if val actually has to be val, and key"
+		echo -e "for val in \"${genre[@]}\"; do"
+		echo -e "\tcmd $val"
+		echo "done"
+		echo "iteration over keys"
+		echo -e "for key in \"${!genre[@]}\"; do"
+		echo -e "\tcmd $key"
+		echo "done"
+	;;
+	17)
+		echo -e "set -o noclobber\t# Avoid overlay files (echo \"hi\" > foo)"
+		echo -e "set -o errexit\t# Used to exit upon error, avoiding cascading errors"
+		echo -e "set -o pipefail\t# Unveils hidden failures"
+		echo -e "set -o nounset\t# Exposes unset variables"
+	;;
+	18)
+		echo "need to play with these, for more understanding"
+		echo -e "shopt -s nullglob    # Non-matching globs are removed  ('*.foo' => '')"
+		echo -e "shopt -s failglob    # Non-matching globs throw errors"
+		echo -e "shopt -s nocaseglob  # Case insensitive globs"
+		echo -e "shopt -s dotglob     # Wildcards match dotfiles ("*.sh" => ".foo.sh")"
+		echo -e "shopt -s globstar    # Allow ** for recursive matches ('lib/**/*.rb' => 'lib/a/b/c.rb')"
+		echo -e "Set GLOBIGNORE as a colon-separated list of patterns to be removed from glob matches."
+	
 	;;
 	*)
 		echo "Exiting Script because I have no clue what you typed o.O"
