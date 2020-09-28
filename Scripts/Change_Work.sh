ed91f19ba3ad484ad084d940e1dcc89812677ac2
Bradley Stradling <45749708+Bradley-Stradling@users.noreply.github.com>
2020-09-28 10:29:50 -0500

    Update Change_Work.sh
    adding variable work paths

--- Scripts/Change_Work.sh
+++ Scripts/Change_Work.sh
@@ -6,19 +6,54 @@
 #(")_(")  Lincense: https://unlicense.org/
 #*******************************************************************************
 # (\_/)   This script is intended to change the sourced variable of the gowork
-# (*.*)   alias to the input'd full path.
+# (*.*)   aliases to the input'd full path.
 #(")_(")  
 #*******************************************************************************
 
-read -p "Enter the full path to the new work spot ==>" werkpath
+read -p "Which workpath would you like to change? 1 2 or 3 -->" which_Werk
 
-if [ -d ${werkpath} ];
-  then
-    echo "work_Path=${werkpath}" > ${work_Path_File}
-    echo "work_Path=${werkpath} written to ${work_Path_File}"
-  else
-    echo "Unable to locate ${werkpath}. ${work_Path_File} is unchanged."
+case $which_Werk in
+
+  1)
+    read -p "Enter the full path to the new work spot 1 ==>" werkpath
+    
+    if [ -d ${werkpath} ];
+      then
+       echo "work_Path_1=${werkpath}" > ${work_Path_1}
+       echo "work_Path_1=${werkpath} written to ${work_Path_1}"
+    else
+      echo "Unable to locate ${werkpath}. ${work_Path_1} is unchanged."
     exit 1
-fi
+  fi
+
+  ;;
+  2)
+    read -p "Enter the full path to the new work spot 2 ==>" werkpath
+    
+    if [ -d ${werkpath} ];
+      then
+       echo "work_Path_1=${werkpath}" > ${work_Path_2}
+       echo "work_Path_1=${werkpath} written to ${work_Path_2}"
+     else
+      echo "Unable to locate ${werkpath}. ${work_Path_2} is unchanged."
+     exit 1
+    fi
+  ;;
+  3)
+     read -p "Enter the full path to the new work spot 3 ==>" werkpath
+    
+    if [ -d ${werkpath} ];
+      then
+       echo "work_Path_1=${werkpath}" > ${work_Path_3}
+       echo "work_Path_1=${werkpath} written to ${work_Path_3}"
+     else
+      echo "Unable to locate ${werkpath}. ${work_Path_3} is unchanged."
+     exit 1
+    fi
+  ;;
+  *)
+    echo "Not sure what your trying to do there bub sorry exiting..."
+    exit 1
+  ;;
 
 exec bash
