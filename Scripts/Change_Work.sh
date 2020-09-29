bbf26d9cfdc3f9aabbdf4ee0d05e49106f0eb3ed
Bradley Stradling <45749708+Bradley-Stradling@users.noreply.github.com>
2020-09-28 23:17:27 -0500

    Update Change_Work.sh
    tested as is and functional!!

--- Scripts/Change_Work.sh
+++ Scripts/Change_Work.sh
@@ -16,44 +16,44 @@ case $which_Werk in
 
   1)
     read -p "Enter the full path to the new work spot 1 ==>" werkpath
-    
+
     if [ -d ${werkpath} ];
       then
        echo "work_Path_1=${werkpath}" > ${work_Path_1}
        echo "work_Path_1=${werkpath} written to ${work_Path_1}"
-    else
-      echo "Unable to locate ${werkpath}. ${work_Path_1} is unchanged."
-    exit 1
-  fi
-
+        else
+          echo "Unable to locate ${werkpath}. ${work_Path_1} is unchanged."
+          exit 1
+    fi
   ;;
   2)
     read -p "Enter the full path to the new work spot 2 ==>" werkpath
-    
+
     if [ -d ${werkpath} ];
       then
-       echo "work_Path_1=${werkpath}" > ${work_Path_2}
-       echo "work_Path_1=${werkpath} written to ${work_Path_2}"
-     else
-      echo "Unable to locate ${werkpath}. ${work_Path_2} is unchanged."
-     exit 1
+       echo "work_Path_2=${werkpath}" > ${work_Path_2}
+       echo "work_Path_2=${werkpath} written to ${work_Path_2}"
+        else
+          echo "Unable to locate ${werkpath}. ${work_Path_2} is unchanged."
+          exit 1
     fi
   ;;
   3)
      read -p "Enter the full path to the new work spot 3 ==>" werkpath
-    
+
     if [ -d ${werkpath} ];
       then
-       echo "work_Path_1=${werkpath}" > ${work_Path_3}
-       echo "work_Path_1=${werkpath} written to ${work_Path_3}"
+       echo "work_Path_3=${werkpath}" > ${work_Path_3}
+       echo "work_Path_3=${werkpath} written to ${work_Path_3}"
      else
-      echo "Unable to locate ${werkpath}. ${work_Path_3} is unchanged."
-     exit 1
+        echo "Unable to locate ${werkpath}. ${work_Path_3} is unchanged."
+        exit 1
     fi
   ;;
   *)
     echo "Not sure what your trying to do there bub sorry exiting..."
     exit 1
   ;;
+esac
 
 exec bash
