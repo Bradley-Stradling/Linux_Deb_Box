8e607c6e49e4e12097ac4da50fc40ca5d6dc0451
Bradley Stradling <45749708+Bradley-Stradling@users.noreply.github.com>
2020-10-07 11:53:22 -0500

    Update Deploy_Tools.sh
    need to test menu and flagging. need to add do the things, conceptually need to categorize and create list, like in 2d array, so install method can be pulled along with name, not sure there yet, but definitely don't want to hardcode the install for each similarly installed tool. other option different lists, apt list vs python list vs foo

--- Scripts/Deploy_Tools.sh
+++ Scripts/Deploy_Tools.sh
@@ -44,59 +44,59 @@ echo "# Added path to my tools directorys." >> .bashrc
 echo "export PATH=\"{path_Tools_Dir}:\$PATH\"" >> .bashrc
 }
 
-main_Menu
+menu_Choice=0
+flag=0
+flag_Option=('filler' 'List' 'Install' 'Update' 'Remove')
 note=0
+main_Menu() {
 if [[ ${note} == 0 ]]; then
   echo "Note: these are community tools and are not mine unless directly stated."
   echo "Tool accredidations and sources can be found within Tool_Accredidation in the Repo"
   $note=1 # theres likely a few ways to do this want to try this for now
 fi
+echo "Welcome to Deploy_Tools.sh what would you like to do?"
+# flag list, install, update or remove, else exit
+echo -e "\(0\) To exit the script"
+echo -e "\(1\) To list a set of tools."
+echo -e "\(2\) To install a set of tools"
+echo -e "\(3\) To update a set of tools"
+echo -e "\(4\) To remove a set of tools"
+}
 
 tool_List_Menu() {
-echo "Note: these are community tools and are not mine unless directly stated."
-echo "Tool accredidations and sources can be found within Tool_Accredidation in the Repo"
 echo -e "\(0\) To exit the script"
-echo -e "\(1\) ALL THE TOOLS"
-echo -e "\(2\) CTF tools"
-echo -e "\(3\) Penetration testing tools"
-echo -e "\(4\) Bug bounty tools"
-echo -e "\(5\) OSINT tools"
-echo -e "\(6\) Editing tools \(code/text\)" # need to check if / needs to be escaped
+echo -e "\(1\) flag_Option[${flag}] ALL THE TOOLS"
+echo -e "\(2\) flag_Option[${flag}] CTF tools"
+echo -e "\(3\) flag_Option[${flag}] Penetration testing tools"
+echo -e "\(4\) flag_Option[${flag}] Bug bounty tools"
+echo -e "\(5\) flag_Option[${flag}] OSINT tools"
+echo -e "\(6\) flag_Option[${flag}] Editing tools \(code/text\)" # need to check if / needs to be escaped
 }
 
 getouttahere() {
 echo -e "Exiting script!"
 exit 0
 }
+flag=0
+get_Main_Menu_Choice() {
+read -p "Which set of tools would you like to deploy? --> " menu_Choice
 
-tool_Set_Choice=0
-get_Tool_List_Choice() {
-read -p "Which set of tools would you like to deploy? --> " tool_Set_Choice
-
-case ${tool_Set_Choice} in
-
+case ${menu_Choice} in
   1)
-    echo " Your choice was ${tool_Set_Choice}"
-    # do I need to actively return ${tool_Set_Choice} or will the variable be changed by the function input outside of the function
-    # will find out during testing
+    echo " Your choice was ${menu_Choice}"
+    flag=${menu_Choice}
   ;;
   2)
-    echo " Your choice was ${tool_Set_Choice}"
+    echo " Your choice was ${menu_Choice}"
+    flag=${menu_Choice}
   ;;
   3)
-    echo " Your choice was ${tool_Set_Choice}"
+    echo " Your choice was ${menu_Choice}"
+    flag=${menu_Choice}
   ;;
   4)
-    echo " Your choice was ${tool_Set_Choice}"
-  ;;
-  5)
-    echo " Your choice was ${tool_Set_Choice}"
-  ;;
-  6)
-    echo " Your choice was ${tool_Set_Choice}"
-  ;;
-  7)
-    echo " Your choice was ${tool_Set_Choice}"
+    echo " Your choice was ${menu_Choice}"
+    flag=${menu_Choice}
   ;;
   *)
     echo " Your choice was not indicated in the menu options, what're you doing o.O"
@@ -105,13 +105,49 @@ case ${tool_Set_Choice} in
 esac
 }
 
-while [[ ${tool_Set_Choice} != 0 ]]; do
+get_Tool_List_Choice() {
+read -p "Which set of tools would you like to deploy? --> " menu_Choice
+
+case ${menu_Choice} in
+  1)
+    echo " Your choice was 'flag_Option[${flag}]' ${tool_Set_Choice}"
+  ;;
+  2)
+    echo " Your choice was 'flag_Option[${flag}]' ${menu_Choice}"
+  ;;
+  3)
+    echo " Your choice was 'flag_Option[${flag}]' ${menu_Choice}"
+  ;;
+  4)
+    echo " Your choice was 'flag_Option[${flag}]' ${menu_Choice}"
+  ;;
+  5)
+    echo " Your choice was 'flag_Option[${flag}]' ${menu_Choice}"
+  ;;
+  6)
+    echo " Your choice was 'flag_Option[${flag}]' ${menu_Choice}"
+  ;;
+  7)
+    echo " Your choice was 'flag_Option[${flag}]' ${menu_Choice}"
+  ;;
+  *)
+    echo " Your choice was not indicated in the menu options, what're you doing o.O"
+    getouttahere
+  ;;
+esac
+}
+
+while [[ ${menu_Choice} != 0 ]]; do
 
 main_Menu
+
+get_Main_Menu_Choice
 
 tool_List_Menu
 
 get_Tool_List_Choice
+
+# do_The_Things
 
 done
 
