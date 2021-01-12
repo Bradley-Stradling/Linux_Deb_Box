9bd48f30966d1787c4e479bbe3e14dfec9cc5be5
Bradley Stradling <45749708+Bradley-Stradling@users.noreply.github.com>
2021-01-12 08:34:38 -0600

    Update Resolution_Set.sh

--- Scripts/Resolution_Set.sh
+++ Scripts/Resolution_Set.sh
@@ -27,7 +27,7 @@ echo -e "Executing \"xrandr --newmode "1920x1080"  172.80 1920 2040 2248 2576 10
 xrandr --newmode 1920x1080 172.80  1920 2040 2248 2576  1080 1081 1084 1118  -HSync +Vsync
 
 #uncomment out this if broke
-#read -p "Please enter the name of the output in xrandr to add the new mode to. -->" output_Name
+read -p "Please enter the name of the output in xrandr to add the new mode to. -->" output_Name
 
 echo -e "Executing \"xrandr --addmode ${output_Name} \"1920x1080\"\""
 
